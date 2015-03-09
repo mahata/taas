854b3c351f93f511bb5f732ecb8d104292a32e8d
Yasunori Mahata <mahata777@gmail.com>
2015-03-08 18:42:28 -0700

    Add some configuration regarding MySQL

--- taas/taas/settings.py
+++ taas/taas/settings.py
@@ -56,12 +56,23 @@ WSGI_APPLICATION = 'taas.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'taas',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
     }
 }
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.7/topics/i18n/
